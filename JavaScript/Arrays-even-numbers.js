b5f04ec1e69a8a8340ca418e3b123e7d450d7ab8
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-01-09 07:40:14 +0530

    Create an array from given array which has only even numbers

---
+++ JavaScript/Arrays-even-numbers.js
@@ -0,0 +1,5 @@
+//create an array from an a given array which contain only the even numbers
+//using map,filter and reduce function
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+var array1 = array.filter(function(a){ return a%2 == 0})
+console.log(array1);
